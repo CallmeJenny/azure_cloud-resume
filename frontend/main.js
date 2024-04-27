eaa3d1cfdec3ec9a6bdab88ddc1469d49713319f
CallmeJenny <jennifer.krohn@outlook.de>
2024-04-27 14:48:29 +0200

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,15 +2,16 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/VisitorCounter';
+const functionApiURL = "https://getvisitorcount-cloudresume.azurewebsites.net/api/VisitorCounter?code=Z7MIFSpHtURFtJCCdNJ7vkTksoPbLlH4e0UE4FN_7mxNAzFuJH5FWQ=="
+const localFunctionApi = "http://localhost:7071/api/VisitorCounter";
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiURL).then(response => {
         return response.json()
     }).then(response =>{
-        console.log("Website called function Api.");
-        count =  response.count;
+        console.log("Website called function API.");
+        count = response.visitorcount;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
